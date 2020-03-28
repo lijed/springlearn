09e2789183090e1057a30468198c8a7cb916d7a9
JedLi <enlbi@163.com>
2020-03-28 23:36:03 +0800

    Initial commit

---
+++ src/test/java/com/me/springlearn/SpringlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.springlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
